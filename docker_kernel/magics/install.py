40de1fc9a694e5b7db71bd672afb05ecc310b466
Marco Lorenz <lorenz369@outlook.de>
2023-06-21 00:19:02 +0200

    Add line breaks for install magic

--- docker_kernel/magics/install.py
+++ docker_kernel/magics/install.py
@@ -25,15 +25,22 @@ class Install(Magic):
         package = " ".join(self._args[1:])
         match self._args[0].lower():
             case "apt-get":
-                code = f"RUN apt-get update && apt-get install -y {package} && rm -rf /var/lib/apt/lists/*"
+                code = f"RUN apt-get update && apt-get install -y**PACKAGES**{package}**PACKAGES**&& rm -rf /var/lib/apt/lists/*"
             case "conda":
-                code = f"RUN conda update -n base -c defaults conda && conda install -y {package} && conda clean -afy"
+                code = f"RUN conda update -n base -c defaults conda && conda install -y**PACKAGES**{package}**PACKAGES**&& conda clean -afy"
             case "npm":
-                code = f"RUN npm install -g npm@latest && npm install {package} && npm cache clean --force"
+                code = f"RUN npm install -g npm@latest && npm install**PACKAGES**{package}**PACKAGES**&& npm cache clean --force"
             case "pip":
-                code = f"RUN pip install --upgrade pip && pip install {package} && rm -rf /var/lib/apt/lists/*"
+                code = f"RUN pip install --upgrade pip && pip install**PACKAGES**{package}**PACKAGES**&& rm -rf /var/lib/apt/lists/*"
             case other:
                 return "Package manager not available (currently available: apt-get)"
-        self._kernel.set_payload("set_next_input", code, True)
-        code = self._kernel.create_build_stage(code)
+        self._kernel.set_payload("set_next_input", self._set_layout(code, "\n"), True)
+        code = self._kernel.create_build_stage(self._set_layout(code, "\\"))
         self._kernel.build_image(code)
+
+    def _set_layout(self, code: str, newline_character: str) -> str:
+        code = code.split("**PACKAGES**")
+        code[1] = code[1].replace(" ", f"{newline_character}\t")
+        code = " ".join(code)
+        code = code.replace("&&", f"&&{newline_character}")
+        return code
