af4b358fa5545eb3694bea0fa4e1a881380261c0
ZoeLi0525 <1558079116@qq.com>
2023-07-15 21:05:28 +0200

    using temporary directory as build context instead of current working directory

--- docker_kernel/kernel.py
+++ docker_kernel/kernel.py
@@ -1,3 +1,6 @@
+import shutil
+import tempfile
+
 import docker
 import io
 import json
@@ -7,7 +10,7 @@ from ipykernel.kernelbase import Kernel
 from .magics.magic import Magic
 from .utils.notebook import get_cursor_frame
 from.magics.helper.errors import MagicError
-from docker.errors import APIError
+from docker.errors import APIError, BuildError
 
 # The single source of version truth
 __version__ = "0.0.1"
@@ -30,8 +33,10 @@ class DockerKernel(Kernel):
         self._api = docker.APIClient(base_url='unix://var/run/docker.sock')
         self._sha1: str | None = None
         self._payload = []
-        self._intermedia_images = {}
-        self._current_alias: str | None = None
+        self._index_to_image_id = {}
+        self._alias_to_index = {}
+        self._current_alias: str | int | None = None
+        self._code: str | None = None
 
     
     @property
@@ -76,7 +81,8 @@ class DockerKernel(Kernel):
         ####################
         # Docker execution
         try:
-            code = self.create_build_stage(code)
+            self._code = code.strip()
+            code = self.create_build_stage(self._code)
             self.build_image(code)
             return {'status': 'ok', 'execution_count': self.execution_count, 'payload': self._payload, 'user_expression': {}}
         except APIError as e:
@@ -124,52 +130,110 @@ class DockerKernel(Kernel):
         -->
         COPY --from=[image id] [src] [dest]
         """
-        self.start_a_new_layer(code)
         code = self.replace_base_image_with_id(code)
         if self._sha1 is not None:
             code = f"FROM {self._sha1}\n{code}"
         return code
 
     def replace_base_image_with_id(self, code):
-        if '--from=' in code:
-            code_seg = code.split(" ")
-            contain_image = [s for s in code_seg if "--from" in s]
-            image_alias = ''.join(contain_image).split("=")[1]
-            base_image_id = list(self._intermedia_images.values())[int(image_alias)] if image_alias.isdigit() \
-                else self._intermedia_images[image_alias]
-            code = f"{code_seg[0]} --from={base_image_id} {' '.join(code_seg[2:])}"
+        code_seg = code.lower().strip().split(" ")
+
+        try:
+            contain_image = next(x for x in code_seg if x.startswith('--from'))
+        except StopIteration:
+            return code
+        image_alias = ''.join(contain_image).split("=")[1]
+        try:
+            if image_alias.isdigit():
+                base_image_id = self._index_to_image_id[int(image_alias)]
+            else:
+                base_image_id = self._index_to_image_id[self._alias_to_index[image_alias]]
+        except KeyError or IndexError:
+            self.send_response("Can't find the base image")
+        code = f"{code_seg[0]} --from={base_image_id} {' '.join(code_seg[2:])}"
         return code
 
     def start_a_new_layer(self, code):
-        if code.startswith("FROM"):
+        if code.lower().startswith('from'):
             try:
-                alias = code.split('as')[1].strip()
-            except:
-                alias = len(self._intermedia_images)
+                _from, *remain = code.split(' ')
+            except ValueError:
+                pass
+            # index = len(self._index_to_image_id)
+            if type(self._current_alias) is int:
+                index = self._current_alias + 1
+            elif type(self._current_alias) is str:
+                index = self._alias_to_index[self._current_alias] + 1
+            else:
+                index = 0
+            if len(remain) > 1 and remain[1] == 'as':
+                alias = remain[2]
+                self._alias_to_index[alias] = index
+            else:
+                alias = index
+            self._index_to_image_id[index] = None
             self._current_alias = alias
-            self._intermedia_images[alias] = ''
 
     def build_image(self, code):
         """ Build docker image by passing input to the docker API."""
 
         f = io.BytesIO(code.encode('utf-8'))
         logs = []
-        current_working_directory = os.getcwd()
-        dockerfile_path = os.path.join(current_working_directory, 'Dockerfile')
-        with open(dockerfile_path, 'w') as df:
-            df.write(code)
-        for logline in self._api.build(path=current_working_directory, dockerfile=dockerfile_path, rm=True):
-            loginfo = json.loads(logline.decode())
+        tmp_dir = tempfile.mkdtemp()
+        try:
+            self.copy_cwd_to_tmp_dir(tmp_dir)
+            dockerfile_path = self.create_dockerfile(code, tmp_dir)
+        except Exception as e:
+            self.send_response(str(e))
 
+        self.start_a_new_layer(self._code)
+        for logline in self._api.build(path=tmp_dir,dockerfile=dockerfile_path, rm=True):
+            loginfo = json.loads(logline.decode())
+            if 'error' in loginfo:
+                self.send_response(f'error:{loginfo["error"]}\n')
+                self.delete_current_stage()
             if 'aux' in loginfo:
                 self._sha1 = loginfo['aux']['ID']
-                image_id = self._sha1.split(':')[1][:12]
-                self._intermedia_images[self._current_alias]=image_id
-
             if 'stream' in loginfo:
                 log = loginfo['stream']
                 if log.strip() != "":
                     self.send_response(log)
+        self.save_current_stage()
+        shutil.rmtree(tmp_dir)
+
+    def copy_cwd_to_tmp_dir(self, tmp_dir):
+        if os.path.exists(tmp_dir):
+            shutil.rmtree(tmp_dir)
+            shutil.copytree(os.getcwd(), tmp_dir)
+
+    def create_dockerfile(self, code, tmp_dir):
+        dockerfile_path = os.path.join(tmp_dir, 'Dockerfile')
+        dockerfile = open(dockerfile_path, 'w+')
+        dockerfile.write(code)
+        dockerfile.close()
+        return dockerfile_path
+
+    def save_current_stage(self):
+        image_id = self._sha1.split(':')[1][:12]
+        if type(self._current_alias) is int:
+            self._index_to_image_id[self._current_alias] = image_id
+        else:
+            self._index_to_image_id[self._alias_to_index[self._current_alias]] = image_id
+
+    def delete_current_stage(self):
+        if type(self._current_alias) is int:
+            del self._index_to_image_id[self._current_alias]
+        else:
+            del self._index_to_image_id[self._alias_to_index[self._current_alias]]
+            del self._alias_to_index[self._current_alias]
+        last_stage_index = list(self._index_to_image_id.keys())[-1]
+        last_stage_alias = ''.join({i for i in self._alias_to_index if self._alias_to_index[i] == last_stage_index})
+        # self.send_response(f'last_stage_index:{last_stage_index}')
+        # self.send_response(f'last_stage_alias:{last_stage_alias}')
+        if not last_stage_alias:
+            self._current_alias = last_stage_index
+        else:
+            self._current_alias = last_stage_alias
 
     def send_response(self, content_text, stream=None, msg_or_type="stream", content_name="stdout"):
         stream = self.iopub_socket if stream is None else stream
