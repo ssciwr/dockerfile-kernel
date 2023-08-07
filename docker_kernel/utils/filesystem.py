5a530eeac057b8ca4d5503fe4122db21a9a00b75
Marvin Weitz <weitz_marvin@outlook.de>
2023-08-07 16:31:17 +0200

    Restructure temp directory handling

--- docker_kernel/utils/filesystem.py
+++ docker_kernel/utils/filesystem.py
@@ -8,14 +8,9 @@ def create_dockerfile(code: str, directory: str, filename="Dockerfile"):
         dockerfile.write(code)
     return dockerfile_path
 
-
 def copy_files(src: str, dest: str):
     try:
         shutil.copytree(src, dest, dirs_exist_ok=True)
+        return True
     except shutil.Error as e:
-        self.send_response(str(e))
-def remove_tmp_dir(tmp_dir: tempfile.TemporaryDirectory):
-    try:
-        tmp_dir.cleanup()
-    except Exception as e:
-        self.send_response(str(e))
+        return e
