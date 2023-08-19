56e6bcdbdf74b0c0b7250787e78b2a97ab9a9385
Marvin Weitz <weitz_marvin@outlook.de>
2023-08-20 00:20:53 +0200

    Fix kernel bug

--- docker_kernel/kernel.py
+++ docker_kernel/kernel.py
@@ -117,9 +117,9 @@ class DockerKernel(Kernel):
 
         ####################
         # Docker execution
-            self.build_image(code)
-            # self.send_response(f"temp dir:{self._tmp_dir.name}\n")
-            return {'status': 'ok', 'execution_count': self.execution_count, 'payload': self._payload, 'user_expression': {}}
+        self.build_image(code)
+        # self.send_response(f"temp dir:{self._tmp_dir.name}\n")
+        return {'status': 'ok', 'execution_count': self.execution_count, 'payload': self._payload, 'user_expression': {}}
 
     def create_build_stage(self, code):
         """
@@ -273,8 +273,6 @@ class DockerKernel(Kernel):
             if e.explanation is not None:
                 self.send_response(str(e.explanation))
         else:
-            self.send_response(str(e))
-
             self._save_build_stage(code, self._sha1)
 
     def _save_build_stage(self, code, image_id):
