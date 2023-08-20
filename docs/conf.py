3223202596ea482f98f5e7dd46ebd0542c7473d9
Marvin Weitz <weitz_marvin@outlook.de>
2023-08-21 00:09:39 +0200

    Setup docstring auto documentation

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,15 @@ project = 'Dockerfile Kernel'
 copyright = '2023, Dominic Kempf, Marco Lorenz, Shuangshuang Li, Marvin Weitz'
 author = 'Dominic Kempf, Marco Lorenz, Shuangshuang Li, Marvin Weitz'
 
+# -- Docstring configuration --------------------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
