5406a4a34b416560a779286988f9548e72f1b620
Marvin Weitz <weitz_marvin@outlook.de>
2023-10-08 21:16:38 +0200

    Reinstate tdd

--- test/test_image_ids.py
+++ test/test_image_ids.py
@@ -1,16 +1,14 @@
-import sys
 import pytest
 import os
 
 from utils import generateKernelId, generateDockerId
 
-@pytest.mark.parametrize("Dockerfile_dir", (d for d in os.listdir(os.path.join(os.path.dirname(__file__), "test_envs"))))
+@pytest.mark.parametrize("Dockerfile_dir", os.listdir(os.path.join(os.path.dirname(__file__), "test_envs")))
 def test_image_ids(Dockerfile_dir):
     test_path = os.path.join(os.path.dirname(__file__), "test_envs", Dockerfile_dir)
-    test_directories = [os.path.join(test_path, d) for d in next(os.walk(test_path))[1]]
     
-    for test_directory in test_directories:
-        dockerfile_name = next(f for f in os.listdir(test_directory) if os.path.isfile(os.path.join(test_directory, f)) and f.lower().endswith("dockerfile"))
-        kernel_id = generateKernelId(test_directory, dockerfile_name)
-        docker_id = generateDockerId(test_directory, dockerfile_name)
-        assert kernel_id == docker_id, "Kernel Id and Docker Id should be the same"
+    dockerfile_name = next(f for f in os.listdir(test_path) if os.path.isfile(os.path.join(test_path, f)) and f.lower().endswith("dockerfile"))
+    
+    kernel_id = generateKernelId(test_path, dockerfile_name)
+    docker_id = generateDockerId(test_path, dockerfile_name)
+    assert kernel_id == docker_id, "Kernel Id and Docker Id should be the same"
